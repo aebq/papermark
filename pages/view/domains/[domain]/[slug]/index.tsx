26ff03c5823fcbbf9fc39f9d88f0a6db95df414b
aebq <contact@barq.ch>
2026-06-19 11:02:18 +0200

    Add files via upload

--- pages/view/domains/[domain]/[slug]/index.tsx
+++ pages/view/domains/[domain]/[slug]/index.tsx
@@ -451,9 +451,7 @@ function ViewPageInner({
             imageUrl={meta.metaImage ?? null}
             url={meta.metaUrl ?? ""}
           />
-          <div className="flex h-screen items-center justify-center">
-            <LoadingSpinner className="h-20 w-20" />
-          </div>
+          <DocumentLoader />
         </>
       );
     }
@@ -532,9 +530,7 @@ function ViewPageInner({
             imageUrl={meta.metaImage ?? null}
             url={meta.metaUrl ?? ""}
           />
-          <div className="flex h-screen items-center justify-center">
-            <LoadingSpinner className="h-20 w-20" />
-          </div>
+          <DocumentLoader />
         </>
       );
     }
